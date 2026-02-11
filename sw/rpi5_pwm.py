1d451cda3e88d4ca5b80e83fd983f52afadb4b2d
juan <21977733+juanmed@users.noreply.github.com>
2026-02-12 00:05:48 +0900

    Some adjustments

--- sw/rpi5_pwm.py
+++ sw/rpi5_pwm.py
@@ -2,10 +2,13 @@ from rpi_hardware_pwm import HardwarePWM
 from time import sleep
 
 # Using RPI5 so channel=2 for gpio18
-pwm = HardwarePWM(pwm_channel=2, hz=60, chip=0)
-pwm.start(100) # full duty cycle
-sleep(2)
-pwm.change_duty_cycle(50)
-pwm.change_frequency(25_000)
-sleep(2)
+print("Setup HW PWM")
+pwm = HardwarePWM(pwm_channel=2, hz=20_000, chip=0)
+pwm.start(5) # full duty cycle
+print("60hz, 95%")
+sleep(5)
+pwm.change_duty_cycle(10)
+#pwm.change_frequency(25_000)
+print("25khz, 50%")
+sleep(5)
 pwm.stop()

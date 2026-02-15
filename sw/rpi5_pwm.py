838633759e1bc4d215acc535e5796fabaec08a49
juanmed <21977733+juanmed@users.noreply.github.com>
2026-02-16 01:49:12 +0900

    use infinite loop inside tasks

--- sw/rpi5_pwm.py
+++ sw/rpi5_pwm.py
@@ -1,23 +1,24 @@
 # Inspired from https://docs.sunfounder.com/projects/umsk/en/latest/05_raspberry_pi/pi_lesson17_rotary_encoder.html
 
 from rpi_hardware_pwm import HardwarePWM
-from time import sleep
 import asyncio
+from contextlib import suppress
 from gpiozero import RotaryEncoder, Button
 
 async def pwm_ramp(pwm: HardwarePWM) -> None:
-    # Ramp duty cycle up
-    print("Ramp up!")
-    for i in range(0, 20, 5):    
-        print(f"DT: {i}%")
-        pwm.change_duty_cycle(i)
-        await asyncio.sleep(1)
+    while True:
+        # Ramp duty cycle up
+        print("Ramp up!")
+        for i in range(0, 20, 5):
+            print(f"DT: {i}%")
+            pwm.change_duty_cycle(i)
+            await asyncio.sleep(1)
 
-    print("Ramp down!")
-    for i in range(20, 0, -5):   
-        print(f"DT: {i}%")
-        pwm.change_duty_cycle(i)
-        await asyncio.sleep(1)
+        print("Ramp down!")
+        for i in range(20, 0, -5):
+            print(f"DT: {i}%")
+            pwm.change_duty_cycle(i)
+            await asyncio.sleep(1)
 
 class Encoder:
 
@@ -29,12 +30,14 @@ class Encoder:
         self.button = Button(22)
 
     async def read_step(self)->None:
-        print(f"Current step: {self.encoder.steps}")
-        await asyncio.sleep(0.05)
+        while True:
+            print(f"Current step: {self.encoder.steps}")
+            await asyncio.sleep(0.05)
     
     async def read_button(self)->None:
-        if self.button.is_pressed:
-            print("Button pressed!")  # Print message on button press
+        while True:
+            if self.button.is_pressed:
+                print("Button pressed!")
             await asyncio.sleep(0.1)
 
 
@@ -43,19 +46,22 @@ async def main():
     print("Setup HW PWM @ 20khz")
     pwm = HardwarePWM(pwm_channel=2, hz=20_000, chip=0)
     pwm.start(0) # stop
-    sleep(1)
+    await asyncio.sleep(1)
 
     encoder = Encoder()
+    pwm_task = asyncio.create_task(pwm_ramp(pwm))
+    step_task = asyncio.create_task(encoder.read_step())
+    button_task = asyncio.create_task(encoder.read_button())
+    tasks = (pwm_task, step_task, button_task)
 
     try:
-        while True:
-            t1 = asyncio.create_task(pwm_ramp(pwm))
-            t2 = asyncio.create_task(encoder.read_step())
-            t3 = asyncio.create_task(encoder.read_button())
-            await t1
-            await t2
-            await t3
+        await asyncio.gather(*tasks)
     finally:
+        for task in tasks:
+            task.cancel()
+        for task in tasks:
+            with suppress(asyncio.CancelledError):
+                await task
         pwm.stop()
         print("Stopping pwm...")
 
